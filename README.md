5f3ddeee16a05795a73bb9e4789c150949b88126
a6srivastava <a6srivastava@ucsd.edu>
2023-05-28 22:36:43 -0700

    lab finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lab 8 - Starter
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+    - Answer: I would choose the first option, automate a github action that tests the code before it pushes to the repository. If the code does not pass the automated tests It should send notificiation to the committer and not push the code to the repo.
+2) Would you use an end to end test to check if a function is returning the correct output?
+    - Answer: End to end will test all functionality, creating a smaller test specific to the function would likely be more appropriate.
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+    -Answer: Yes, unit tests would be useful to test the "message" feature of a messaging app. The "message" will several distinct various parts of the process such as input validation, encrypting, sending, receiving, notifying triggering. And we can use tests to detect flaws in these.
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+    -yes, this is a very easy use of testing since we need to restrict a simple boundry of length.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,98 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Write unit tests for the following functions: isPhoneNumber, isEmail, isStrongPassword and isDate, and isHexColor 
+// You should write at least 6 tests for each function the tests should test edge cases as well as normal cases, and thoughroughly test the functions
+// You should use the expect function to test the functions
+// You should use the toBe function to test the functions
+describe('Unit Test Me Functions', () => {
+    // Unit tests for isPhoneNumber function
+    describe('isPhoneNumber', () => {
+      test('should return true for valid phone number', () => {
+        expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+        expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+        expect(functions.isPhoneNumber('111(123) 456-7890')).toBe(true);
+      });
+  
+      test('should return false for invalid phone number', () => {
+        expect(functions.isPhoneNumber('2')).toBe(false);
+        expect(functions.isPhoneNumber('(123 4$#56-7890')).toBe(false);
+        expect(functions.isPhoneNumber('*****')).toBe(false);
+      });
+  
+      test('should return false for empty string', () => {
+        expect(functions.isPhoneNumber('')).toBe(false);
+      });
+    });
+  
+    // Unit tests for isEmail function
+    describe('isEmail', () => {
+      test('should return true for valid email', () => {
+        expect(functions.isEmail('test@example.com')).toBe(true);
+        expect(functions.isEmail('absrivas@gmail.com')).toBe(true);
+      });
+  
+      test('should return false for invalid email', () => {
+        expect(functions.isEmail('test@example')).toBe(false);
+        expect(functions.isEmail('test@.com')).toBe(false);
+      });
+  
+      test('should return false for empty string', () => {
+        expect(functions.isEmail('')).toBe(false);
+      });
+    });
+  
+    // Unit tests for isStrongPassword function
+    describe('isStrongPassword', () => {
+      test('should return true for strong password', () => {
+        expect(functions.isStrongPassword('Password1')).toBe(true);
+        expect(functions.isStrongPassword('LoberJuice')).toBe(true);
+
+      });
+  
+      test('should return false for weak password', () => {
+        expect(functions.isStrongPassword('pass+')).toBe(false);
+        expect(functions.isStrongPassword('1Password')).toBe(false);
+      });
+  
+      test('should return false for password with special character', () => {
+        expect(functions.isStrongPassword('Password@1')).toBe(false);
+      });
+    });
+  
+    // Unit tests for isDate function
+    describe('isDate', () => {
+      test('should return true for valid date', () => {
+        expect(functions.isDate('12/31/2023')).toBe(true);
+        expect(functions.isDate('10/11/1023')).toBe(true);
+      });
+  
+      test('should return false for invalid date', () => {
+        expect(functions.isDate('113/32/2023')).toBe(false);
+        expect(functions.isDate('12/31/23')).toBe(false);
+        expect(functions.isDate('/31/23')).toBe(false);
+      });
+  
+      test('should return false for empty string', () => {
+        expect(functions.isDate('')).toBe(false);
+      });
+    });
+  
+    // Unit tests for isHexColor function
+    describe('isHexColor', () => {
+      test('should return true for valid color', () => {
+        expect(functions.isHexColor('#FFFFFF')).toBe(true);
+        expect(functions.isHexColor('#FFF')).toBe(true);
+        expect(functions.isHexColor('FFFFFF')).toBe(true);
+      });
+  
+      test('should return false for invalid color', () => {
+        expect(functions.isHexColor('#FFFFF')).toBe(false);
+        expect(functions.isHexColor('#ZZZ')).toBe(false);
+      });
+  
+      test('should return false for empty string', () => {
+        expect(functions.isHexColor('')).toBe(false);
+      });
+    });
+  });
